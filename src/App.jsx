2216ef289e0b7bcca61e78ff61532c11aafb4110
ImranSanvi <imranmohammed5220@gmail.com>
2025-10-17 10:11:27 +0600

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import navImg from './assets/logo.png'
+import dollarImg from './assets/DoubleDollar.png'
 import './App.css'
 
 function App() {
@@ -8,8 +10,21 @@ function App() {
 
   return (
     <>
-      <div className="navbar bg-primary text-primary-content">
-        <button className="btn btn-ghost text-xl">daisyUI</button>
+      <div className="flex flex-col md:flex-row my-[30px] mx-[40px] md:mt-[50px] md:mx-[140px] justify-between items-center">
+        <img src={navImg} alt="" />
+        <div className='flex flex-col md:flex-row gap-4 justify-between items-center my-4 md:my-0'>
+          <div className='flex gap-4'>
+            <a href="" className='font-normal text-[16px]'>Home</a>
+            <a href="" className='font-normal text-[16px]'>Fixture</a>
+            <a href="" className='font-normal text-[16px]'>Teams</a>
+            <a href="" className='font-normal text-[16px]'>Schedules</a>
+          </div>
+          <div className="flex justify-between items-center gap-2 border-1 border-green-800 p-2 rounded-[10px]">
+            <span>0</span>
+            <span>Coin</span>
+            <img src={dollarImg} alt="" />
+          </div>
+        </div>
       </div>
     </>
   )
