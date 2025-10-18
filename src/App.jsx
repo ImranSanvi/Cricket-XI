208738167e2ed6382e45e19f1da7fa2c422319dc
ImranSanvi <imranmohammed5220@gmail.com>
2025-10-18 09:41:08 +0600

    card show

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { Suspense, useState } from 'react'
 
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
@@ -6,8 +6,17 @@ import Banner from './Components/Banner/Banner'
 import AvailablePlayers from './Components/AvailablePlayers/AvailablePlayers'
 import SelectedPlayers from './Components/SelectedPlayers/SelectedPlayers'
 
+
+
+const fetchPlayers = async () =>{
+  const res = await fetch('/players.json')
+  return res.json()
+}
+
 function App() {
   const [count, setCount] = useState(0)
+
+  const playersPromise = fetchPlayers()
 
   return (
     <>
@@ -16,7 +25,13 @@ function App() {
 
       <Banner></Banner>
 
-      <AvailablePlayers></AvailablePlayers>
+      <Suspense fallback={
+          <div className='flex justify-center items-center'>
+            <span className="loading loading-bars loading-xl"></span>
+          </div>
+        }>
+        <AvailablePlayers playersPromise={playersPromise}></AvailablePlayers>
+      </Suspense>
 
       <SelectedPlayers></SelectedPlayers>
 
